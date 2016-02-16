ca07cf41b84f2af04625a4ce12c65a106bf54ccc
Casper Knudsen <casper60@gmail.com>
2016-02-16 19:52:52 +0100

    Added example data

--- simpleMenu.js
+++ simpleMenu.js
@@ -2,6 +2,20 @@ function SimpleMenu(options) {
 	
 	'use strict';
 	
+	var exampleData = [
+		{ title: 'Frontpage', href: '#/frontpage' },
+		{ title: 'Photo albums', children: [
+			{ title: '1950-1990', href: '#/photos/1950-1990/' },
+			{ title: '1991-2000', href: '#/photos/1991-2000/' },
+			{ title: '2001-2014', children: [
+				{ title: 'Summer', href: '#/photos/2001-2014/summer/' },
+				{ title: 'Winther', href: '#/photos/2001-2014/winther/' }
+			] }
+		] },
+		{ title: 'About', href: '#/about.php' },
+		{ title: 'Contact', href: '#/contact' }
+	];
+	
 	
 	
 	(function constructor() {
